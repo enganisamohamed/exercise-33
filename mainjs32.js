df656740ac6ebb2ebb628682badd960b0f134668
Anisa Mohamed <anisa@example.com>
2026-04-04 22:12:50 -0700

    adding exercise 32

---
+++ mainjs32.js
@@ -0,0 +1,16 @@
+// Select by ID
+const header = document.querySelector("#header");
+
+// Select all paragraphs
+const paragraphs = document.querySelectorAll(".text");
+
+// Select paragraph inside div
+const insideDiv = document.querySelector(".container .text");
+
+// Select button
+const button = document.querySelector("#btn");
+
+console.log(header);
+console.log(paragraphs);
+console.log(insideDiv);
+console.log(button);
